d3f8a0432855669dcbe32789131a4304ca9c3940
Sudhir Singh <sudhirsingh3923@gmail.com>
2025-03-06 22:50:10 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://hackathon-pccoe.onrender.com',
+  baseURL: 'https://hackathon-pccoe.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
